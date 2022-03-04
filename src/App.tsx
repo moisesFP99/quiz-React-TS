6d592a27cd1009d68caae4932eb8a0e51ca8044a
Moises <moisesxd8@gmail.com>
2022-03-03 23:14:42 -0300

    setup folders and starting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+//Components
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+    
+  }
+
   return (
-    <div className="App">Quiz</div>
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
   );
 }
 
