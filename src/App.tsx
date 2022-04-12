a039f020f57088ada3566ef95970933a11f0ed6e
Moises <moisesxd8@gmail.com>
2022-04-11 23:03:27 -0300

    setting url and difficulty

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-//Components
+import { fetchQuizQuestions } from './API';
+// Components
 import QuestionCard from './components/QuestionCard';
+// Types
+import { Difficulty } from './API';
 
 const TOTAL_QUESTIONS = 10;
 
@@ -13,6 +16,8 @@ function App() {
     const [userAnswers, setUserAnswers] = useState([]);
     const [score, setScore] = useState(0);
     const [gameOver, setgameOver] = useState(true);
+
+    console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
 
 
   }
@@ -34,14 +39,14 @@ function App() {
       </button>
       <p className='score'>Score:</p>
       <p>Loading Questions ...</p>
-      <QuestionCard 
+      {/* <QuestionCard 
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
